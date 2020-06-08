36484288f1cfedd0ecf2c70c61782385ef0b9d39
AnthoLbz <anthony.lasbleiz@gmail.com>
2020-06-08 15:23:35 +0200

    created router + rename App

---
+++ src/Router.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Landing from "./components/Landing";
+
+const Router = () => {
+  return (
+    <>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+      </Switch>
+    </>
+  );
+};
+
+export default Router;
