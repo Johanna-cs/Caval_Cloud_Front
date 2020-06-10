57df1f5a4da42c1e63c1fbc23d629a0936c2a6e6
Johanna Cas <johannacas@iMac-de-Johanna.local>
2020-06-10 16:18:35 +0200

    adding link for future routes on home button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../App.css'
 import horse from './SVG-icons/horse-icon.SVG'
 import helmet from './SVG-icons/bombe-icon.svg'
@@ -20,30 +20,41 @@ function Home(props) {
        <div className='HomePage'> 
             <h5 className='welcome'> Bienvenue sur Cavalcloud ! </h5>
             <p className='welcomeText'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod. </p>
-                <div className='searchHorse'>
-                    <img className='logoHome' src={horse} alt='horse icon'/>
-                    <p className='textBtn'> Chercher un équidé </p>
-                </div>
 
-                <div className='searchRider'>
-                    <img className='logoHome' src={helmet} alt='helmet icon'/>
-                    <p className='textBtn'> Chercher un cavalier </p>
-                </div>
-                
+                <Link to='/searchHorse' >
+                    <div className='searchHorse'>
+                        <img className='logoHome' src={horse} alt='horse icon'/>
+                        <p className='textBtn'> Chercher un équidé </p>
+                    </div>
+                </Link>
+
+                <Link to='/searchRider' >
+                    <div className='searchRider'>
+                        <img className='logoHome' src={helmet} alt='helmet icon'/>
+                        <p className='textBtn'> Chercher un cavalier </p>
+                    </div>
+                </Link>
+
+                <Link to='/postRider' >
                 <div className='postrider'>
                     <img className='logoHome' src={boot} alt='boot icon'/>
                     <p className='textBtn'> Poster une annonce cavalier </p>
                 </div>
+                </Link>
 
-                <div className='postHorse'>
-                    <img className='logoHome' src={horseFrame} alt='horse frame icon'/>
-                    <p className='textBtn'> Poster une annonce cheval </p>
-                </div>
+                <Link to='/postHorse' >
+                    <div className='postHorse'>
+                        <img className='logoHome' src={horseFrame} alt='horse frame icon'/>
+                        <p className='textBtn'> Poster une annonce cheval </p>
+                    </div>
+                </Link>
 
-                <div className='aboutUs'>
-                    <img className='logoHome' src={cavalCloudLogo} alt='caval cloud logo'/>
-                    <p className='textUs'> Qui sommes-nous ? </p>
-                </div>
+                <Link to='/aboutUs' >
+                    <div className='aboutUs'>
+                        <img className='logoHome' src={cavalCloudLogo} alt='caval cloud logo'/>
+                        <p className='textUs'> Qui sommes-nous ? </p>
+                    </div>
+                </Link>
 
        </div>
     )
