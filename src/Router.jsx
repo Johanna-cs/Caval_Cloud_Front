8dfc4d539c924865a8c3c9907677439f4cbb7743
Johanna Cas <johannacas@iMac-de-Johanna.local>
2020-06-11 09:10:22 +0200

    component home css ok, link and routes initialized

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Landing from "./components/Landing";
+// import Landing from "./components/Landing";
 import Home from "./components/Home"
 
 const Router = () => {
   return (
     <>
       <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/Home" component={Home} />
+        {/* <Route exact path="/" component={Landing} /> */}
+        <Route exact path="/home" component={Home} />
       </Switch>
     </>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import { Link } from 'react-router-dom';
 import '../App.css'
-import horse from './SVG-icons/horse-icon.SVG'
+import horse from './SVG-icons/horse-icon.svg'
 import helmet from './SVG-icons/bombe-icon.svg'
 import boot from './SVG-icons/botte-icon.svg'
 import horseFrame from './SVG-icons/horse-in-cadre-icon.svg'
-import cavalCloudLogo from './SVG-icons/cavalcloud-logo-OK.svg'
+import cavalCloudLogo from './SVG-icons/cavalcloud-logoTest.jpg'
 
 
 function Home(props) {
@@ -21,37 +21,37 @@ function Home(props) {
             <h5 className='welcome'> Bienvenue sur Cavalcloud ! </h5>
             <p className='welcomeText'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod. </p>
 
-                <Link to='/searchHorse' >
-                    <div className='searchHorse'>
-                        <img className='logoHome' src={horse} alt='horse icon'/>
+                <Link to='/searchHorse' style={{ textDecoration: "none" }} >
+                    <div className='homeBtn' >
+                        <img className='logoHorse' src={horse} alt='horse icon'/>
                         <p className='textBtn'> Chercher un équidé </p>
                     </div>
                 </Link>
 
-                <Link to='/searchRider' >
-                    <div className='searchRider'>
-                        <img className='logoHome' src={helmet} alt='helmet icon'/>
+                <Link to='/searchRider' style={{ textDecoration: "none" }} >
+                    <div  className='homeBtn' >
+                        <img className='logoHelmet' src={helmet} alt='helmet icon'/>
                         <p className='textBtn'> Chercher un cavalier </p>
                     </div>
                 </Link>
 
-                <Link to='/postRider' >
-                <div className='postrider'>
-                    <img className='logoHome' src={boot} alt='boot icon'/>
+                <Link to='/postRider' style={{ textDecoration: "none" }} >
+                <div  className='homeBtn'>
+                    <img className='logoboot' src={boot} alt='boot icon'/>
                     <p className='textBtn'> Poster une annonce cavalier </p>
                 </div>
                 </Link>
 
-                <Link to='/postHorse' >
-                    <div className='postHorse'>
-                        <img className='logoHome' src={horseFrame} alt='horse frame icon'/>
+                <Link to='/postHorse' style={{ textDecoration: "none" }} >
+                    <div  className='homeBtn'>
+                        <img className='logoHorseframe' src={horseFrame} alt='horse frame icon'/>
                         <p className='textBtn'> Poster une annonce cheval </p>
                     </div>
                 </Link>
 
-                <Link to='/aboutUs' >
-                    <div className='aboutUs'>
-                        <img className='logoHome' src={cavalCloudLogo} alt='caval cloud logo'/>
+                <Link to='/aboutUs' style={{ textDecoration: "none" }} >
+                    <div  className='AboutUSBtn'>
+                        <img className='logoCaval' src={cavalCloudLogo} alt='caval cloud logo'/>
                         <p className='textUs'> Qui sommes-nous ? </p>
                     </div>
                 </Link>
