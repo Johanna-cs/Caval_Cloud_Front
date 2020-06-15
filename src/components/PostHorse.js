b9aa6abcd2f8fb54b15f1fc0c8b4661c2a3ba643
nadir-13016 <nadir-13016@hotmail.fr>
2020-06-15 14:35:47 +0200

    starting feature

---
+++ src/components/PostHorse.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./posthorse.css";
+import { Link } from "react-router-dom";
+import logo from "../SVG-icons/cavalcloud-logo.png";
+
+const Login = () => {
+  return (
+    <div >
+        <h2>Poster une Annonce Cheval</h2>
+        <p>Localisation</p>
+        <hr/>
+        <p>Prix</p>
+        <hr/>
+
+        <hr/>
+        <hr/>
+
+     
+    </div>
+  );
+};
+export default Login;
