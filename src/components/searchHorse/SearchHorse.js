9e4999468977ca6628f219d9aca59c53ed803c0f
Johanna Cas <johannacas@iMac-de-Johanna.local>
2020-06-15 18:28:46 +0200

    price section added with some light css

--- src/components/searchHorse/SearchHorse.js
+++ src/components/searchHorse/SearchHorse.js
@@ -18,12 +18,12 @@ const SearchHorse = () => {
         <div className='searchHorse_page'>
 
             <div className='searchHorse_loc'>
-            <h4>Localisation </h4>
+                <h4>Localisation </h4>
                 <input id='locInput' placeholder='Localisation'/>
-                <div className='searchHorse_loc2'>
                 <Button id='toggle_btn_chev'color="transparent" onClick={toggle} style={{ marginBottom: '1rem' }}><svg class="bi bi-chevron-down" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
                     </svg></Button>
+                <div className='searchHorse_loc2'>
                 <Collapse isOpen={isOpen}>
                     <h4>Localisation  :</h4>
                     <p className='loc_text'>Dans un rayon autour de : </p>
@@ -45,9 +45,30 @@ const SearchHorse = () => {
 
 
             <div className='searchHorse_price' >
-                 
-
+            
+                <Button id='toggle_btn_chev'color="transparent" onClick={toggle} style={{ marginBottom: '1rem' }}><svg class="bi bi-chevron-down" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                        </svg></Button>
+                <div className='searchHorse_price2'>
+                <Collapse isOpen={isOpen}>
+                    <h4> Prix mensuel maximum : </h4>
+                    <input id='priceInput' placeholder='prix/mois'/>
+                            <h4> Devise : </h4>
+                            <div className='selectDevise'> 
+                                <input className='deviseInput' name='selectEur' type="radio" value="option1" checked/>
+                                <label className='deviseLabel' for='selectEur'> Euros - € </label>
+                                <hr />
+                                <input className='deviseInput' name='selectLiv' type="radio" value="option2" checked/>
+                                <label className='deviseLabel' for='selectLiv'> Livre Sterling - £ </label>
+                                <hr />
+                                <input className='deviseInput' name='selectDol' type="radio" value="option3" checked/>
+                                <label className='deviseLabel' for='selectDol'> Dollar - $ </label>
+                                <hr />
+                            </div>
+                    </Collapse>
+                    </div>
             </div>
+            
         </div>
     )
 }
