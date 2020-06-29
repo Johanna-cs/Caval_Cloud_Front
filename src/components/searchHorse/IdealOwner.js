36fff6e282ad0ff47152c390eb15d1ad2449619e
Johanna Cas <johannacas@iMac-de-Johanna.local>
2020-06-29 12:09:44 +0200

    some new integration on search horse component

--- src/components/searchHorse/IdealOwner.js
+++ src/components/searchHorse/IdealOwner.js
@@ -1,14 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import { Collapse, Button, Input } from 'reactstrap'
+import React from 'react'
+import SelectButton from '../common/SelectButton'
+import RadioCheck from '../common/RadioCheck'
+
 
 const IdealOwner = () => {
-    
-    const [isOpen, setIsOpen] = useState(true)
-    const toggle = () => setIsOpen(!isOpen)
       
     return (
 
-
+<>
 
         <div className='searchHorse_idealRider'>
             <div className='rider_age'>
@@ -20,31 +19,35 @@ const IdealOwner = () => {
             <div className='rider_caracter'>
                     <h5> Caractère :</h5>
                 <div className='select_caracter'>
-                        <Button className='selectBtn' size='lg'>Introverti</Button>
-                        <Button className='selectBtn' size='lg'>Sociable</Button>
-                        <Button className='selectBtn' size='lg'>Extraverti</Button>
-                        <Button className='selectBtn' size='lg'>Solitaire</Button>
-                    </div>
+                    <RadioCheck 
+                    RadioCheckText1='Introverti' 
+                    RadioCheckText2='Sociable'
+                    RadioCheckText3='Extraverti' 
+                    RadioCheckText4='Solitaire' />
+
+                    
+                </div>
             </div>
                 <hr />
             <div className='rider_communication'>
                 <h5> Fréquence de communication :</h5>
                     <div className='select_communication'>
-                        <Button className='selectBtn2' size='lg'>Moins d'une fois par semaine</Button>
-                        <Button className='selectBtn2' size='lg'>Une fois par semaine</Button>
-                        <Button className='selectBtn2' size='lg'>Plus d'une fois par semaine</Button>
+                    <SelectButton textBtn={"Moins d'une fois par semaine"}/>
+                    <SelectButton textBtn={"Une fois par semaine"}/>
+                    <SelectButton textBtn={"Plus d'une fois par semaine"}/>
                     </div>
              </div>
                 <hr />
             <div className='rider_horseWork'>
                 <h5> Travail du cheval :</h5>
                     <div className='select_horseWork'>
-                        <Button className='selectBtn2' size='lg'>Ouvert à la nouveauté</Button>
-                        <Button className='selectBtn2' size='lg'>Normal</Button>
-                        <Button className='selectBtn2' size='lg'>Cadré</Button>
+                    <SelectButton textBtn={"Ouvert à la nouveauté"}/>
+                    <SelectButton textBtn={"Normal"}/>
+                    <SelectButton textBtn={"Cadré"}/>
                     </div>
             </div>
         </div>
+        </>
     )}
 
 export default IdealOwner
